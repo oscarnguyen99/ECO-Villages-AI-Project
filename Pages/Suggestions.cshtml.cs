d0bcf5774662520fcd1237568ff1339f6d250663
JJlrocks42 <ludickjuan@gmail.com>
2021-05-23 14:35:14 +0200

    Juan Second Update

---
+++ Pages/Suggestions.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blazor1.Pages
+{
+    public class SuggestionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
