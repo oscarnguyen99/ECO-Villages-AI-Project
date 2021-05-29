f53934966c71190d63bb9329940c3b615f12bebf
JJlrocks42 <ludickjuan@gmail.com>
2021-05-29 14:01:43 +0200

    Juan Update 4

---
+++ Pages/Database_Handler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using static Microsoft.JSInterop.IJSRuntime;
+using Microsoft.JSInterop;
+using System.Threading;
+
+namespace Blazor1.Pages
+{
+    public class Database_Handler:IJSRuntime
+    {
+        public static string ConnectionDB = "Data Source=JUAN-PC\\SQLEXPRESS01;Initial Catalog=EcoDorp;Integrated Security=True";
+        public static SqlConnection Con = new SqlConnection();
+
+        public Database_Handler()
+        {
+        }
+
+        public static void OpenConn() 
+        {
+            Con = new SqlConnection(ConnectionDB);
+
+            try
+            {
+                Con.Open();
+                Con.Close();
+            }
+            catch (Exception)
+            {
+                async Task ErrorMessage_DB_Connection() 
+                {
+                    Database_Handler handler = new Database_Handler();
+                    await handler.InvokeVoidAsync(identifier: "ErrorMessageDB_Connection");
+                }
+
+
+            }
+
+        }
+
+        ValueTask<TValue> IJSRuntime.InvokeAsync<TValue>(string identifier, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        ValueTask<TValue> IJSRuntime.InvokeAsync<TValue>(string identifier, CancellationToken cancellationToken, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- wwwroot/js/Intop.js
+++ wwwroot/js/Intop.js
@@ -1,4 +1,8 @@
-﻿function createAlert() {
+﻿const { reload } = require("browser-sync");
+var counter = 0;
+
+
+function createAlert() {
     alert("Hello there");
 
 }
@@ -11,18 +15,30 @@ function SetElement(id) {
 
 function GenerateBars(datatodisplay, labelstodisplay)
 {
+    var chcontainer = document.getElementById("chCon");
+    chcontainer.style = " z-index: 1;  visibility:visible; Height : 80%; Width: 80%;";
+  
+
+
+    var element = document.getElementById("ftxt");
+    element.innerText = ""
+    var dtd = datatodisplay;
    
     var sel = document.getElementById('graphtype');
-    
+    sel.style = " z-index: 1;  visibility:visible;";
 
     var ctx = document.getElementById('testchart');
+    
+    
+  
+
     new Chart(ctx, {
         type: sel.value,
         data: {
             labels: labelstodisplay, datasetElementType: null,
             datasets: [{
                 label: 'Temp',
-                data: datatodisplay,
+                data: dtd,
                 backgroundColor:[ 'rgba(255,99,132,0.2)', 'rgba(255,99,123,1)',
                 'rgba(54,162,235,1)',
                 'rgba(255,206,86,1)',
@@ -34,7 +50,9 @@ function GenerateBars(datatodisplay, labelstodisplay)
                     'rgba(75,192,192,1)',
                     'rgba(153,102,255,1)',
                     'rgba(255,159,64,1)'],
-                borderWidth: 1
+                borderWidth: 1,
+                pointStyle: "circle",
+                
             }]
 
         },
@@ -59,10 +77,28 @@ function GenerateBars(datatodisplay, labelstodisplay)
 }
 
 
+
+
 function setChartNull() {
-    var getChart = document.getElementById("chart-container");
-    getChart.value = null;
-    alert("sup");
+    //var getChart = document.getElementById("chart-container");
+    //getChart.value = null;
+    counter = counter + 1;
+
+    if (counter >= 1)
+    {
+        alert("sup");
+        var chcontainer = document.getElementById("chCon");
+        chcontainer.style = " z-index: 1;  visibility:visible; Height : 80%; Width: 80%;";
+        chcontainer.innerHTML = reload();
+        chcontainer.innerText = "";
+
+        var sel = document.getElementById('graphtype');
+        sel.style = " z-index: 1;  visibility:visible;";
+    }
+
+   // console.log(counter);
+
+
     //getChart.type = "reset";
     //getChart.inputMode = "reset";
     //getChart.innerHTML = null;
@@ -74,6 +110,101 @@ function setChartNull() {
 
 function LogList(PolutionList)
 {
+    console.clear();
     console.log(PolutionList);
+    console.log(PolutionList.data[0].co);
+    console.log(PolutionList.country_code);
+}
+
+
+
+function DisplayCurrentData(PolutionList)
+{
+    var tagH = document.createElement("p");
+    var text = document.createTextNode("City Name:" + PolutionList.city_name);
+    tagH.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tagH);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("timezone:" + PolutionList.data[0].timezone);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("CO:" + PolutionList.data[0].co);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("o3:" + PolutionList.data[0].o3);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("predominant_pollen_type:" + PolutionList.data[0].predominant_pollen_type);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("so2:" + PolutionList.data[0].so2);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("pollen_level_tree:" + PolutionList.data[0].pollen_level_tree);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("pollen_level_weed:" + PolutionList.data[0].pollen_level_weed);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("no2:" + PolutionList.data[0].no2);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("pm25:" + PolutionList.data[0].pm25);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("pollen_level_grass:" + PolutionList.data[0].pollen_level_grass);
+    tag.appendChild(text);
+    var element = document.getElementById("ftxt");
+    element.appendChild(tag);
+
+    var element = document.getElementById("bd");
+    element.style = " overflow:visible; align-items:center; max - height: 80 %; max - width: 80 %;";
+
+    var sel = document.getElementById('graphtype');
+    sel.style = " z-index: -1;  visibility:hidden;";
+    var chcontainer = document.getElementById("chCon");
+    chcontainer.style = " z-index: -1;  visibility:hidden; Height : 0%; Width: 0%;";
+
+
+    LogList(PolutionList);
+
+
+}
+
+
+
+
+function ErrorMessageDB_Connection()
+{
+    alert("Error no Database Connection");
 
 }
